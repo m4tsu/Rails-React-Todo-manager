211c31ccae4175f48111e4d1bce87ebd9f607e31
m4tsu <matsurec0147@gmail.com>
2019-10-08 00:42:00 +0900

    add task.rb

--- api/app/models/task.rb
+++ api/app/models/task.rb
@@ -5,7 +5,22 @@ class Task < ApplicationRecord
   end
 
   def create
-    @tasks = Task.all
-    render json: @tasks
+    @task = Task.create(params[:task])
+    render json: @task
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update_attributes(params[:task])
+    render json: @task
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    if @task.destroy
+      head :no_content, status: :ok
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
   end
 end
