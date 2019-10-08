b0989dc8af2da96ec4946537918fcf3ac1e18ee1
m4tsu <matsurec0147@gmail.com>
2019-10-08 19:54:37 +0900

    add validation to task

--- api/app/models/task.rb
+++ api/app/models/task.rb
@@ -1,3 +1,5 @@
 class Task < ApplicationRecord
-
+  validates :title, presence: true, length: {maximum: 256}
+  validates :content, presence: true
+  validates :status, presence: true, numericality: true
 end
--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_07_143918) do
+ActiveRecord::Schema.define(version: 2019_10_08_105214) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title", limit: 256, null: false
     t.text "content", null: false
-    t.integer "status", limit: 2, default: 0
+    t.integer "status", limit: 2, default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
